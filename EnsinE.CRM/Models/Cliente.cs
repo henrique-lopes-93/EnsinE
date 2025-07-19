5c493991664912896f6d39dbadf0e064b81bb9c6
Henrique Lopes <lopesoliveirahenrique7@gmail.com>
2025-07-19 13:57:50 -0300

    feat: Clientes OK

--- EnsinE.CRM/Models/Cliente.cs
+++ EnsinE.CRM/Models/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace EnsinE.CRM.Models
 {
@@ -6,24 +7,29 @@ namespace EnsinE.CRM.Models
     {
         public int ClienteId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "O Nome Completo é obrigatório")]
+        [Display(Name = "Nome Completo")]
         public string NomeCompleto { get; set; } = string.Empty;
 
-        [Phone]
+        [Phone(ErrorMessage = "Telefone inválido")]
         public string Telefone { get; set; } = string.Empty;
 
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
         public string Email { get; set; } = string.Empty;
 
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "O desconto deve estar entre 0 e 100%")]
+        [Display(Name = "Desconto (%)")]
         public decimal Desconto { get; set; }
 
+        [Required(ErrorMessage = "O Vendedor é obrigatório")]
         public string Vendedor { get; set; } = string.Empty;
 
-        // FK
-        public int ProdutoId { get; set; }
+        [Required(ErrorMessage = "Selecione um produto")]
+        [Display(Name = "Produto")]
+        public int? ProdutoId { get; set; }
 
-        // Navegação
+
+        [ValidateNever]
         public Produto Produto { get; set; } = null!;
     }
 }
---
+++ EnsinE.CRM/Views/Clientes/PorProduto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnsinE.CRM.Views.Clientes
+{
+    public class PorProdutoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
