9f47c4798c9eb5c493d37baa32202e0087c45c7b
Henrique Lopes <lopesoliveirahenrique7@gmail.com>
2025-07-19 22:08:00 -0300

    removidos arquivos de suporte ao desenvolvimento

--- EnsinE.CRM/Controllers/ClientesController.cs
+++ EnsinE.CRM/Controllers/ClientesController.cs
@@ -26,24 +26,6 @@ namespace EnsinE.CRM.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Clientes/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cliente = await _context.Clientes
-                .Include(c => c.Produto)
-                .FirstOrDefaultAsync(m => m.ClienteId == id);
-            if (cliente == null)
-            {
-                return NotFound();
-            }
-
-            return View(cliente);
-        }
 
         // GET: Clientes/Create
         public IActionResult Create()
@@ -61,6 +43,7 @@ namespace EnsinE.CRM.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("ClienteId,NomeCompleto,Telefone,Email,Desconto,Vendedor,ProdutoId")] Cliente cliente)
         {
+
             Console.WriteLine($"ProdutoId recebido: {cliente.ProdutoId}");
             if (!ModelState.IsValid)
             {
@@ -97,8 +80,7 @@ namespace EnsinE.CRM.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ClienteId,NomeCompleto,Telefone,Email,Desconto,Vendedor,ProdutoId")] Cliente cliente)
         {
-            if (id != cliente.ClienteId)
-                return NotFound();
+            cliente.ClienteId = id;
 
             if (!ModelState.IsValid)
             {
